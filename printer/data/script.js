4c04718f36ff4bf70d2ecf9c838bedc93893ecc5
calciumchan <calcium.ca.chan@gmail.com>
2024-03-13 16:07:41 -0700

    refactor

--- printer/data/script.js
+++ printer/data/script.js
@@ -1,107 +1,217 @@
 /*----------------------------------------------------------------------------------------------------
 
-	VARIABLES
+	TEMPLATE CONSTRUCTORS
 
 ----------------------------------------------------------------------------------------------------*/
-var cardnum = 0;
 
-/*----------------------------------------------------------------------------------------------------
+function create_element_from_template(id){
 
-	CARD LOADERS
-
-----------------------------------------------------------------------------------------------------*/
-//LOADS CARD, TAKES SRC AS INPUT
-function loadcard(src){
-	//GET PREVIEW BAY
-	const previews = document.getElementById("previewbay");
-	if(src != ""){
-		//CREATE IMAGE
-		let img = document.createElement("img");
-		img.src = src;
-		img.classList.add("previewcard");
-
-		img.id = "card_image" + cardnum;
-
-		//CREATE HOLDER
-		let holder = document.createElement("div");
-		holder.classList.add("cardholder")
-		holder.appendChild(img);
-
-		//ADD CONTROLS
-		holder.appendChild(create_controls(cardnum));
-
-		//SET ID1
-
-		holder.id = "card_holder" + cardnum;
-		cardnum++;
-
-		//ADD TO DOM
-		previews.appendChild(holder);
-	}
+	//CREATES AN ELEMENT BASED ON THE TEMPLATE DATA IN THE HTML DOCUMENT
+	const template = document.getElementById(id);
+	return template.content.cloneNode(true);
 }
 
-/*----------------------------------------------------------------------------------------------------
+//const ele = create_element_from_template("template_menu_base");
 
-	CARD CONTROLS
+//document.body.appendChild(ele);
 
-----------------------------------------------------------------------------------------------------*/
-//CREATES CARD CONTROLS
-function create_controls(number){
-	//CONTROLS
-	let control_holder = document.createElement("div");
+function construct_template(id){
+	let data = templatedata[id];
 
-	//CREATE CARD SELECTION BUTTON
-	let button_select = document.createElement("input");
-	button_select.type = "checkbox"
-
-	control_holder.appendChild(button_select);
-
-	//CREATE DELETION BUTTON
-	let button_delete = document.createElement("input");
-	button_delete.type = "button";
-	button_delete.value = "DELETE";
-	button_delete.onclick = function(){delete_card("card_holder" + number);};
-
-	control_holder.appendChild(button_delete);
-
-	control_holder.appendChild(document.createElement("br"));
-
-	//CREATE CARD COUNT BUTTONS
-	let button_count = document.createElement("input");
-	button_count.type = "number"
-	button_count.value = 1;
-	button_count.style.width = "3rem"
-	button_count.id = "card_counter" + number
-
-	control_holder.appendChild(button_count);
+	//CREATE ELEMENT
+	let element = create_element_from_template(data.template);
 	
-	return control_holder;
-}
 
-//DELETE CARD
-function delete_card(id){
-	console.log("TEST");
-	document.getElementById(id).remove();
+	let wrapper = undefined;
+	if(data.wrapper != undefined){
+		//CREATE WRAPPER
+		if(data.wrapper.id != undefined){
+			//FROM TEMPLATE ID
+			wrapper = construct_template(data.wrapper.id);
+		}else if(data.wrapper.template != undefined){
+			//FROM HTML TEMPLATE
+			wrapper = create_element_from_template(data.wrapper.template);
+		}
+	}
+
+	//ADD PARTS
+	if(data.parts != undefined){
+		for(let i = 0; i < data.parts.length; i++){
+			//CREATE PART
+			let part = undefined;
+			if(data.parts[i].id != undefined){
+				//FROM TEMPLATEID
+				part = construct_template(data.parts[i].id);
+			}else if(data.parts[i].template != undefined){
+				//FROM HTML TEMPLATE
+				part = create_element_from_template(data.parts[i].template);
+			}
+
+			//ADD TO ELEMENT IF NOT UNDEFINED
+			if(data.parts[i].addto != undefined){
+				element.getElementById(data.parts[i].addto).appendChild(part);
+			}else{
+				element.appendChild(part);
+			}
+		}
+	}
+
+	//RETURNS
+	if(wrapper != undefined){
+		console.log(wrapper)
+		//ADD TO WRAPPER
+		if(data.wrapper.addto != undefined){
+			//ADD TO ELEMENT IF NOT UNDEFINED
+			wrapper.getElementById(data.wrapper.addto).appendChild(element);
+		}else{
+			wrapper.appendChild(element);
+		}
+
+		//RETURN WRAPPER
+		return wrapper;
+	}
+
+	//RETURN ELEMENT IF NO WRAPPER EXISTS
+	return element;
 }
 
 /*----------------------------------------------------------------------------------------------------
 
-	LOAD FILES
+	MENUS
 
 ----------------------------------------------------------------------------------------------------*/
-//GRABS FILES FROM INPUT
-function loadfrominput(inputid){
-	//GET INPUT
-	const input = document.getElementById(inputid);
-	if(input.value != ""){
-		//LOOP THROUGH CARDS
-		const list = input.value.split("\n");
-		for(let i = 0; i< list.length; i++){
-			loadcard(list[i]);
-		}
-		//loadcard(input.value);
-		input.value = "";
+
+let active_menus = [];
+
+function menu_open(type){
+	//CONSTRUCT THE MENU
+	let menu = construct_template(type);
+
+	//ADD TO LIST
+	active_menus.push(menu.getElementById("slot_menu_holder"));
+
+	//ADD TO DOM
+	document.getElementById("menus").appendChild(menu)
+
+	return active_menus[active_menus.length -1];
+
+}
+
+function menu_close(){
+	active_menus.pop().remove();
+}
+
+function menu_close_all(){
+	while(active_menus.length > 0){
+		menu_close();
 	}
+}
+
+/*----------------------------------------------------------------------------------------------------
+
+	IMPORTERS
+
+----------------------------------------------------------------------------------------------------*/
+function import_files(){
+	//CREATE INPUT
+	let input = document.createElement("input");
+	input.type = "file";
+	input.multiple = true;
+	input.accept = "image/jpeg, image/png, image/jpg, image/gif";
+
+	input.onchange = e => import_files_after(e);
+
+
+	input.click();
+
+	
+}
+
+function import_files_after(e){
+
+	//LOOP THROUGH FILES
+	for(let i = 0; i < e.target.files.length; i++){
+		var fr = new FileReader(); 
+		fr.readAsDataURL(e.target.files[i]);
+		fr.addEventListener("load",function(){
+			add_card(this.result);
+		})
+	}
+
+	//CLEAR MEMORY ?
+	e.target.remove();
+
+	//close menus
+	if(e.target.files.length > 0){
+		menu_close_all();
+	}
+}
+
+function import_files_url(elementid){
+	let text = document.getElementById(elementid).value;
+
+	if(text != undefined){
+		//MAKE SURE TEXT EXISTS AND THEN SEPARATE INTO A LIST
+		let list = text.split("\n");
+		for(let i = 0; i < list.length; i++){
+			if(list[i] != ""){
+				add_card(list[i]);
+			}
+		}
+	}
+
+	menu_close_all();
+}
+
+/*----------------------------------------------------------------------------------------------------
+
+	CARD LOADING
+
+----------------------------------------------------------------------------------------------------*/
+
+let card_list = [];
+
+let card_number = 0;
+
+function add_card(src){
+	//CREATE CARD HOLDER
+	let element = create_element_from_template("template_card_holder")
+
+	//SET IMAGE SOURCE
+	element.getElementById("card_image").src = src;
+
+	//GRAB THE CARD HOLDER
+	let holder = element.getElementById("card_holder");
+
+	//SET ANIMATION SPEED
+	holder.style.animationDuration = (0.25 + (Math.random() * .5)) + "s";
+
+	//SET ID
+	holder.id = "card_holder_" + card_number;
+	card_number++;
+
+	//SET DELETE FUNCTION
+	holder.getElementsByClassName("card_button_delete")[0].onclick = function(){
+		remove_card(holder.id);
+	}
+	
+	//PUSH TO ARRAY
+	card_list.push(holder);
+
+	document.getElementById("main_previewbay").appendChild(element);
+	update_card_preview_scale(document.getElementById("card_preview_scale_slider"));
+}
+
+function remove_card(id){
+	//REMOVE FROM ARRAY
+	for(let i = 0; i < card_list.length; i++){
+		if(card_list[i].id == id){
+			card_list.splice(i,1)
+		}
+	}
+
+	//REMOVE FROM DOM
+	document.getElementById(id).remove();
 }
 
 /*----------------------------------------------------------------------------------------------------
@@ -109,119 +219,128 @@ function loadfrominput(inputid){
 	CARD PRINTING
 
 ----------------------------------------------------------------------------------------------------*/
-function printcards(){
-	//GET THE LIST OF CARDS
-	let cardstoprint = document.body.getElementsByClassName("previewcard");
 
-	//GET PRINTBAY
-	const printbay = document.getElementById("printbay");
+function print_cards(){
+	//CREATE PRINTBAY
+	let printbay = document.createElement("div");
+	printbay.id = "printbay";
+	let onlyselected = false;
+	onlyselected = active_menus.at(-1).getElementsByClassName("button_onlyselected")[0].checked;
 
-	//CLONE ALL THE CARDS THAT ARE LOADED
-	for(let i = 0; i < cardstoprint.length; i++){
+	//ADD CARDS TO PRINTBAY
+	for(let i = 0; i < card_list.length; i++){
+		let selected = card_list[i].getElementsByClassName("is_card_selected")[0].checked;
 
-		//GET NUMBER OF COPIES TO PRINT
-		let amount = document.getElementById("card_counter" + cardstoprint[i].id.split("card_image")[1]).value;
+		if(onlyselected == false || selected == true){
+			let card = card_list[i].getElementsByClassName("card_preview")[0];
+
+			let amount = card_list[i].getElementsByClassName("input_card_count")[0].value;
+
+			for(let j = 0; j < amount; j++){
+				let clone = card.cloneNode(true);
+
+				clone.id = "";
+				clone.className = "printcard";
 		
-		for(let j = 0; j < amount; j++){
-			//CREATE CLONE
-			let clone = cardstoprint[i].cloneNode(true);
-
-			//SET CLONES CLASS
-			clone.classList.replace("previewcard","printcard");
-
-			//ADD CLONES TO PRINTBAY
-			printbay.appendChild(clone);
+				printbay.appendChild(clone );
+			}
 		}
+		
 	}
+
+	//ADD PRINT BAY TO DOM
+	document.body.appendChild(printbay);
 
 	//PRINT
 	window.print();
 
-	//CLEAR PRINTBAY
-	printbay.innerHTML = "";
+	//REMOVE PRINT BAY AND CLOSE MENUS
+	printbay.remove();
+	menu_close_all();
 }
-
-for(let i = 0; i < 18 * 2; i++){
-	//loadcard("https://www.takaratomy.co.jp/products/en.wixoss/card/thumb/WXDi-D04-014[EN].jpg");
-}
-
-
-
-
-
-function loadfiles(){
-	let input = document.getElementById("fileinput");
-	for(let i = 0; i < input.files.length; i++){
-		console.log(input.files[i]);
-
-		var fr = new FileReader(); 
-		fr.readAsDataURL(input.files[i]);
-		fr.addEventListener("load",function(){
-			loadcard(this.result);
-		})
-	}
-}
-
 
 /*----------------------------------------------------------------------------------------------------
 
-	FLOATERS
+	CONNECTION DATA
 
 ----------------------------------------------------------------------------------------------------*/
-function show_floater(id){
-	document.getElementById("floater_holder").style.display = "grid";
-	document.getElementById(id).style.display = "block";
-}
 
-function close_floaters(){
-	document.getElementById("floater_holder").style.display = "none";
-	let elements = document.getElementsByClassName("control_floating");
-	for(let i = 0; i < elements.length; i++){
-		elements[i].style.display = "none";
+const templatedata = {
+	menu_holder:{
+		template:""
+	},
+	menu_base:{
+		template:"template_menu_base",
+		wrapper:{
+			template:"template_menu_holder",
+			addto:"slot_menu_holder"
+		}
+	},
+	menu_import:{
+		template:"template_menu_import",
+		wrapper:{
+			id:"menu_base",
+			addto:"slot_menu_main"
+		},
+		parts:[
+		]
+	},
+	menu_import_urls:{
+		template:"template_menu_import_urls",
+		wrapper:{
+			id:"menu_base",
+			addto:"slot_menu_main"
+		},
+	},
+	menu_import_files:{
+		template:"template_menu_import_files",
+		wrapper:{
+			id:"menu_base",
+			addto:"slot_menu_main"
+		},
+	},
+	menu_export:{
+		template:"template_menu_export",
+		wrapper:{
+			id:"menu_base",
+			addto:"slot_menu_main"
+		},
+		parts:[
+		]
+	},
+	menu_print:{
+		template:"template_menu_print",
+		wrapper:{
+			id:"menu_base",
+			addto:"slot_menu_main"
+		},
+		parts:[
+		]
+	},
+	menu_examine_card:{
+		template:"template_menu_examine_card",
+		wrapper:{
+			id:"menu_base",
+			addto:"slot_menu_main"
+		}
 	}
 }
-
-
-
-function testsave(){
-	let file = new Blob(["help","me","please"],{type:"text"})
-	let url = URL.createObjectURL(file);
-
-	let a = document.createElement("a");
-	a.href = url;
-	a.download = "test"
-	document.body.appendChild(a);
-	a.click();
-}
-
 
 /*----------------------------------------------------------------------------------------------------
 
-	SAVE OUT TO JSON
+	CONTROLS
 
 ----------------------------------------------------------------------------------------------------*/
-function save_json(){
-	//LOOP THROUGH CARDS
-	let data = [];
-	let cardstosave = document.body.getElementsByClassName("previewcard");
-	for(let i = 0; i < cardstosave.length; i++){
-		//CONSTRUCT DATA
-		let inputdata = {};
-		inputdata["src"] = cardstosave[i].src;
-		
-		//LOAD INTO OUR OUTPUT DATA
-		data.push(inputdata);
-	}
 
-	//TEST
-	console.log(data);
+function update_card_preview_scale(button){
+	const value = button.value;
+	document.documentElement.style.setProperty("--card-preview-scale",value);
+}
 
-	//SAVE OUT
-	let file = new Blob([JSON.stringify(data,null,4)],{type:"text/plain"});
-	let url = URL.createObjectURL(file);
-	let a = document.createElement("a");
-	a.href = url;
-	a.download = "cards.txt";
-	//document.body.appendChild(a);
-	a.click();
+function examine_card(element){
+	let menu = menu_open("menu_examine_card");
+
+	let img = menu.getElementsByClassName("menu_examine_card_image");
+
+	img[0].src = element.src
 }
